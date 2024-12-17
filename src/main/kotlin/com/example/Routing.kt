32ff2a79cab95688ec44df425612cf994e1e6ca8
Romain Pouclet <code@romain-pouclet.com>
2024-12-16 18:04:56 -0800

    remove TODO

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -5,23 +5,22 @@ import com.example.generated.controllers.SomeController.Companion.someRoutes
 import com.example.generated.controllers.TypedApplicationCall
 import com.example.generated.models.SomeObj
 import com.example.generated.models.State
+import com.example.generated.models.StateA
+import com.example.generated.models.StateB
+import com.example.generated.models.StateBMode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-
         someRoutes(object: SomeController {
             override suspend fun getSomeObject(call: TypedApplicationCall<SomeObj>) {
-                TODO("Not yet implemented")
+                call.respondTyped(SomeObj(state = StateA()))
             }
 
             override suspend fun sendSomeObject(state: State, call: TypedApplicationCall<SomeObj>) {
-                TODO("Not yet implemented")
+                call.respondTyped(SomeObj(state = state))
             }
         })
     }
